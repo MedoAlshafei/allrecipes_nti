f256ff80ae70c5acad788ceacae44582f54e9a68
MedoAlshafei <elshafey009@gmail.com>
2025-10-22 01:21:41 +0300

    finish banner ui

--- lib/features/home/views/widgets/home_body_builder.dart
+++ lib/features/home/views/widgets/home_body_builder.dart
@@ -1,3 +1,5 @@
+import 'package:allrecipes/features/home/views/widgets/home_content_header_builder.dart';
+import 'package:allrecipes/features/home/views/widgets/trusted_recipe_banner.dart';
 import 'package:flutter/material.dart';
 
 class HomeBodyBuilder extends StatelessWidget {
@@ -9,59 +11,13 @@ class HomeBodyBuilder extends StatelessWidget {
     final width = size.width;
     final height = size.height;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(
-                  "https://www.allrecipes.com/thmb/hQJ9hR2s2rZ1iY-VI0T7R1AWXaA=/400x250/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7482913-texas-chili-ddmfs-4x3-1251-104a7c3b12984d778dd82e6873bb245f.jpg",
-                  width: 400,
-                  height: 250,
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(height: height * 0.01),
-                Text(
-                  "EVERYDAY COOKING",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.grey[600],
-                  ),
-                ),
-                SizedBox(height: height * 0.01),
-                // First 3 Lines in Description
-                Text(
-                  "5 Easy Chili Recipes That Will Never Get Old",
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(height: height * 0.01),
-                // Fill Description
-                Text(
-                  "These easy and comforting chili recipes each have their own spin, but they're all packed with crowd-pleasing flavor.",
-                  maxLines: 6,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey[900],
-                  ),
-                ),
-                SizedBox(height: height * 0.01),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: HomeContentHeaderBuilder(height: height)),
+        SliverToBoxAdapter(
+          child: TrusteRecipeBannerBuilder(height: height, width: width),
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/home/views/widgets/trusted_recipe_banner.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class TrusteRecipeBannerBuilder extends StatelessWidget {
+  const TrusteRecipeBannerBuilder({
+    super.key,
+    required this.height,
+    required this.width,
+  });
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height * 0.12,
+      width: double.infinity,
+      color: Colors.blue[900],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "America's",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: width * 0.02),
+              Text(
+                "#1 Trusted Recipe Resource",
+                style: TextStyle(
+                  color: Colors.amber[400],
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: height / 200),
+          Row(
+            spacing: width * 0.08,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              recipeResourceBuilder(
+                width,
+                height,
+                Color(0xFF2EC5B6),
+                "51K",
+                "Original Recipes",
+              ),
+              recipeResourceBuilder(
+                width,
+                height,
+                Color(0xFFF15025),
+                "7M+",
+                "Ratings & Reviews",
+              ),
+              recipeResourceBuilder(
+                width,
+                height,
+                Color(0xFFE7AB46),
+                "67M",
+                "Home Cooks",
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget recipeResourceBuilder(
+    double width,
+    double height,
+    Color circleColor,
+    String number,
+    String text,
+  ) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 9,
+              child: CircleAvatar(backgroundColor: circleColor, radius: 8),
+            ),
+            SizedBox(width: width * 0.01),
+            Text(
+              number,
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: height * 0.001),
+        Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
