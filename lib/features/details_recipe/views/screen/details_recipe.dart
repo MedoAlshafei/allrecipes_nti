625140e1f5e3e725e213dc959f17bee5c22ad28d
MedoAlshafei <elshafey009@gmail.com>
2025-10-22 12:39:48 +0300

    Add IngredientsBuilder widget for displaying recipe ingredients

--- lib/features/details_recipe/views/screen/details_recipe.dart
+++ lib/features/details_recipe/views/screen/details_recipe.dart
@@ -1,3 +1,4 @@
+import 'package:allrecipes/features/details_recipe/views/widgets/ingredients_builder.dart';
 import 'package:flutter/material.dart';
 
 class DetailsRecipe extends StatelessWidget {
@@ -152,52 +153,6 @@ class DetailsRecipe extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class IngredientsBuilder extends StatelessWidget {
-  const IngredientsBuilder({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final width = size.width;
-    final height = size.height;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Ingredients",
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w800,
-            color: Colors.black,
-          ),
-        ),
-        SizedBox(height: height * 0.02),
-        builderIngredients(width, height),
-        builderIngredients(width, height),
-        builderIngredients(width, height),
-      ],
-    );
-  }
-
-  Widget builderIngredients(double width, double height) {
-    return Row(
-      children: [
-        CircleAvatar(backgroundColor: Colors.orange[800], radius: 5),
-        SizedBox(width: width * 0.02),
-        Text(
-          "2 cups shredded cooked chicken",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w400,
-            color: Colors.black,
-          ),
-        ),
-        SizedBox(height: height * 0.04),
-      ],
     );
   }
 }
